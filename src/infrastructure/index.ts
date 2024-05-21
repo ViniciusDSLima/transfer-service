314b9368ef276d5f8108a89d97c3e4e7dfa37622
Vinicius <vinicius.devjvm@gmail.com>
2024-05-21 13:16:18 -0300

    build: fastify configuration

---
+++ src/infrastructure/index.ts
@@ -0,0 +1,27 @@
+import Fastify from "fastify";
+import {userRoutes} from "./api/routes/user/user.routes";
+import {transferRouter} from "./api/routes/transfer/transfer.routes";
+
+const fastify = Fastify({
+    logger: true
+});
+const server = async() => {
+
+
+
+    const optionsObject = {
+        port: Number(process.env.PORT) || 3000,
+        host: "0.0.0.0"
+    };
+
+    await userRoutes(fastify, optionsObject);
+
+    await transferRouter(fastify, optionsObject);
+
+
+    await fastify.listen(optionsObject);
+
+
+};
+
+server();
