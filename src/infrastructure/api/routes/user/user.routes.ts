74c13019b11304696bbe3709a20e4b3de647dc78
Vinicius <vinicius.devjvm@gmail.com>
2024-05-21 21:11:37 -0300

    jest: add user route

---
+++ src/infrastructure/api/routes/user/user.routes.ts
@@ -0,0 +1,19 @@
+import {FastifyInstance, FastifyPluginAsync} from "fastify";
+import {CreateUserUseCase} from "../../../../application/user/usecases/create/createUser.usecase";
+import {UserRepository} from "../../../../domain/user/repository/user.repository";
+import {InputUserDTO} from "../../../../application/user/usecases/dto/user.dto";
+import {FindUserFactory} from "../../../../application/user/factory/findUser.factory";
+
+export const userRoutes: FastifyPluginAsync = async (fastify: FastifyInstance) => {
+    fastify.post(
+        "/createUser", async (req,reply) => {
+             const createUserUseCase = new CreateUserUseCase(new UserRepository(), FindUserFactory());
+
+                 const data = req.body as InputUserDTO;
+
+                 await createUserUseCase.execute(data);
+
+                 reply.code(201).send({output: data});
+        }
+    );
+};
